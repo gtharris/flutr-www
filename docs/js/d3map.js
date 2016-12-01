4f264fedf36807dcfed5d6cd10557872bd1e1b9f
dagarw <divaagarwal201@gmail.com>
2016-12-01 17:58:46 -0600

    radius

--- docs/js/d3map.js
+++ docs/js/d3map.js
@@ -57,7 +57,7 @@ d3.csv("Breadcrumbs.csv", function(err, data) {
             "stroke-width": 1
         })
         .transition().duration(1000)
-        .attr("r", 6)
+        .attr("r", 5)
 
     circleControl.on("update", function() {
         svg.selectAll("circle.dot").style({
