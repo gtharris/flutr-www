0fb6e8504a29368341089e2cf04f63ce753803d6
GTH <gtharris@iastate.edu>
2016-12-02 11:53:25 -0600

    Hello Diane

--- src/welcome/welcome.go
+++ src/welcome/welcome.go
@@ -10,5 +10,5 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "Welcome from the language of the coming decade - Go!\n")
+        fmt.Fprint(w, "Welcome from the language of the coming decade - Go!\n Hello Diane")
 }
