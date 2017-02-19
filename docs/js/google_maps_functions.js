5de08f280be38a1592fff92f7640335ed3b08839
AndrewDorn <dornadigital@gmail.com>
2017-02-19 17:27:13 -0600

    Updating js to include fix for the center point issue.

--- docs/js/google_maps_functions.js
+++ docs/js/google_maps_functions.js
@@ -1,3 +1,52 @@
+//Appends to the given docElement a pie chart with the population value for each element in the pieTotals array
+function CreatePieChart(docElement, pieTotals)
+{
+	var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+	
+	var color = d3.scaleOrdinal()
+		.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+	
+	var arc = d3.arc()
+		.outerRadius(radius - 10)
+		.innerRadius(0);
+
+	var labelArc = d3.arc()
+		.outerRadius(radius - 40)
+		.innerRadius(radius - 40);
+
+	var pie = d3.pie()
+		.sort(null)
+		.value(function(d) { return d.population; });
+
+	var svg = docElement.append("svg")
+		.attr("width", width)
+		.attr("height", height)
+	  .append("g")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+	
+	var g = svg.selectAll(".arc")
+		.data(pie(pieTotals))
+	.enter().append("g")
+		.attr("class", "arc");
+
+	g.append("path")
+		.attr("d", arc)
+		.style("fill", function(d) { return color(d.data.population); });
+
+	g.append("text")
+		.attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+		.attr("dy", ".35em")
+		.text(function(d) { return d.data.population; });
+
+	function type(d) {
+	  d.population = +d.population;
+	  return d;
+	}
+
+}
+
 //Returns a container with both a map and heatmap layer to be used for visualization. Must be assigned to a spot in the html
 function InitNewMap(center_lat, center_long, html_map_element)
 {
@@ -11,6 +60,111 @@ function InitNewMap(center_lat, center_long, html_map_element)
 		//Additional fields here as needed
 	};
 	return mapData;
+}
+
+function LoadAndProcessAndroidSurveyZipFile(file, onLoadFunction)
+{
+	zip.createReader(new zip.BlobReader(file), function(reader) {
+		reader.getEntries(function(entries) {
+			var pos = FindFileName(entries, "surveys.csv");
+			if(pos == -1)
+				return;
+			entries[pos].getData(new zip.TextWriter(), function(text){
+				var surveys = LoadSurveysFromString(text);
+				
+				var pos = FindFileName(entries, "breadcrumbs.csv")
+				if(pos == -1)
+					return;
+				
+				entries[pos].getData(new zip.TextWriter(), function(text){
+					var breadcrumbs = LoadBreadcrumbsFromString(text);
+					
+					for(i = 0;i < breadcrumbs.length; i++)
+					{
+						var j;
+						for(j = 0; j < surveys.length;j++)
+						{
+							if(surveys[j]._id == breadcrumbs[i].surveyID)
+								break;
+						}
+						if(j >= surveys.length)
+						{
+							alert("Error while processing breadcrumbs, survey ID \"" + breadcrumbs[i] + "\" not found.");
+							return;
+						}
+						surveys[j].breadcrumb_data.push(breadcrumbs[i]);
+					}
+					
+					var pos = FindFileName(entries, "sightings.csv")
+					if(pos == -1)
+						return;
+					
+					entries[pos].getData(new zip.TextWriter(), function(text){
+						var sightings = LoadSightingsFromString(text);
+						
+						for(i = 0;i < sightings.length; i++)
+						{
+							var j;
+							for(j = 0; j < surveys.length;j++)
+							{
+								if(surveys[j]._id == sightings[i].surveyID)
+									break;
+							}
+							if(j >= surveys.length)
+							{
+								alert("Error while processing sightings, survey ID \"" + sightings[i] + "\" not found.");
+								return;
+							}
+							surveys[j].sighting_data.push(sightings[i]);
+						}
+						
+						var pos = FindFileName(entries, "transects.csv")
+						if(pos == -1)
+							return;
+						
+						
+						entries[pos].getData(new zip.TextWriter(), function(text){
+							surveys.transects = LoadTransectsFromString(text);
+							
+							onLoadFunction(surveys);
+						});
+					});
+				});
+			});
+		});
+	}, function(error) {
+		alert(error);
+	});
+}
+
+function BuildJSONFromObject(obj)
+{
+	obj = JSON.stringify(obj);
+	return obj;
+}
+
+function BuildObjectFromJSON(str)
+{
+	str = JSON.parse(str);
+	return str;
+}
+
+function FindFileName(files, name)
+{
+	for(i = 0;i < files.length;i++)
+	{
+		if(files[i].filename == name)
+			return i;
+	}
+	alert("Could not find \"" + name + "\".");
+	return -1;
+}
+
+function LogSurveyInfoAction()
+{
+	return function(survey){
+		console.log(survey);
+	}
 }
 
 //Loads breadcrumbs from the given CSV and parses them into an array where each element is an object in the following format:
@@ -52,6 +206,97 @@ function LoadBreadcrumbsFromString(text)
 				lng: parseFloat(values[5]),
 				accuracy: parseFloat(values[6]),
 				speed: parseFloat(values[7])
+			};
+		}
+	}
+	return text;
+}
+
+function LoadSurveysFromString(text)
+{
+	text = text.split('\n')
+	text.splice(0, 1)
+	for(var i = 0;i < text.length;i++)
+	{
+		if(text[i].length == 0)//blank line, remove it and try again
+		{
+			text.splice(i, 1);
+			i--;
+		}
+		else
+		{
+			var values = ParseCSVLine(text[i]);
+			
+			text[i] = {
+				_id: values[0],
+				type: values[1],
+				name: values[2],
+				start_date: values[3],
+				start_time: values[4],
+				end_date: values[5],
+				end_time: values[6],
+				locationName: values[7],
+				surveyorCount: parseInt(values[8]),//TODO: Make sure this is the correct format
+				surveyorName: ParseCSVLine(values[9]),
+				comment: values[10],
+				habitatType: values[11],
+				habitatCondition: values[12],
+				windSpeed: values[13],
+				habitatCondition: values[14],
+				windSpeed: values[15],
+				radius: values[16],
+				cloudCover: values[17],
+				engagement: values[18],
+				breadcrumbs: values[19],
+				temperature: values[20],
+				upload: values[21],
+				transdiv: values[22],
+				ohumidity: values[23],
+				orain: values[24],
+				opressure: values[25],
+				otemp: values[26],
+				otime_date: values[27],
+				otime_time: values[28],
+				osnow: values[29],
+				owinddir: values[30],
+				owindspeed: values[31],
+				ocity: values[32],
+				ocountry: values[33],
+				ocloudcover: values[34],
+				omaxtemp: values[35],
+				omintemp: values[36],
+				osunrise_date: values[37],
+				osunrise_time: values[38],
+				osunset_date: values[39],
+				osunset_time: values[40],
+				totaltime: values[41],
+				breadcrumb_data: [],
+				sighting_data:[]
+			}
+		}
+	}
+	return text;
+}
+
+function LoadTransectsFromString(text)
+{
+	text = text.split('\n');
+	text.splice(0, 1);//first line is labels
+	for(var i = 0;i < text.length;i++)
+	{
+		if(text[i].length == 0)//blank line, remove it and try again
+		{
+			text.splice(i, 1);
+			i--;
+		}
+		else
+		{	
+			var values = ParseCSVLine(text[i]);
+			
+			text[i] = {
+				_id: parseInt(values[0]),
+				name: values[1],
+				desc: values[2],
 			};
 		}
 	}
@@ -159,38 +404,80 @@ function MapBreadcrumbsToMapAction(map)
 {
 	return function(breadcrumbs){
 		//Add proto polygons and lines
-		var pathCoordinates = [];
-		for(var i = 0;i < breadcrumbs.length;i++)
-		{
-			// var marker = new google.maps.Marker({//Mark the breadcrumb
-				// position: {lat: breadcrumbs[i].lat, lng: breadcrumbs[i].lng},
-				// map: map,
-				// title: breadcrumbs[i]._id.toString()
-			// });
-			
-			pathCoordinates.push({lat: breadcrumbs[i].lat, lng: breadcrumbs[i].lng});//Add breadcrumb to the path
-			
-			// new google.maps.Circle({
-				// strokeColor: '#FF0000',
-				// strokeOpacity: 0.0,
-				// strokeWeight: 2,
-				// fillColor: '#FF0000',
-				// fillOpacity: 0.25,
-				// map: map,
-				// center: pathCoordinates[pathCoordinates.length-1],
-				// radius: 10
-			// });
-		}
-		
-		var surveyPath = new google.maps.Polyline({//Draw the path
-			path: pathCoordinates,
-			geodesic: true,
-			strokeColor: '#FF0000',
-			strokeOpacity: 1.0,
-			strokeWeight: 1
-		});
-		surveyPath.setMap(map);
+		MapBreadcrumbsToMap(breadcrumbs, map);
 	};
+}
+
+function MapBreadcrumbsToMap(breadcrumbs, map)
+{
+	var pathCoordinates = [];
+	for(var i = 0;i < breadcrumbs.length;i++)
+	{
+		// var marker = new google.maps.Marker({//Mark the breadcrumb
+			// position: {lat: breadcrumbs[i].lat, lng: breadcrumbs[i].lng},
+			// map: map,
+			// title: breadcrumbs[i]._id.toString()
+		// });
+		
+		pathCoordinates.push({lat: breadcrumbs[i].lat, lng: breadcrumbs[i].lng});//Add breadcrumb to the path
+		
+		// new google.maps.Circle({
+			// strokeColor: '#FF0000',
+			// strokeOpacity: 0.0,
+			// strokeWeight: 2,
+			// fillColor: '#FF0000',
+			// fillOpacity: 0.25,
+			// map: map,
+			// center: pathCoordinates[pathCoordinates.length-1],
+			// radius: 10
+		// });
+	}
+	
+	var surveyPath = new google.maps.Polyline({//Draw the path
+		path: pathCoordinates,
+		geodesic: true,
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 1
+	});
+	surveyPath.setMap(map);
+}
+
+function MapSightingsToMapAction(map)
+{
+	return function(sightings){
+		MapSightingsToMap(sightings, map)
+	};
+}
+
+function MapSightingsToMap(sightings, map)
+{
+	for(var i = 0;i < sightings.length;i++)
+	{
+		var marker = new google.maps.Marker({//Mark the sighting
+			position: {lat: sightings[i].lat, lng: sightings[i].lng},
+			map: map,
+			title: sightings[i].number + " " + sightings[i].speciesCommon + "(s)"
+		});
+	}
+}
+
+function MapSurveysToMapAction(map)
+{
+	return function(surveys)
+	{
+		var i;
+		for(i = 0;i < surveys.length; i++)
+		{
+			MapSurveyToMap(map, surveys[i]);
+		}
+	}
+}
+
+function MapSurveyToMap(map, survey)
+{
+	MapBreadcrumbsToMap(survey.breadcrumb_data, map);
+	MapSightingsToMap(survey.sighting_data, map);
 }
 
 function GetCenter(points)
@@ -203,11 +490,11 @@ function GetCenter(points)
 	points.forEach(function(point) {
 		if(point.lat > maxLat)
 			maxLat = point.lat;
-		else if(point.lat < minLat)
+		if(point.lat < minLat)
 			minLat = point.lat;
 		if(point.lng > maxLng)
 			maxLng = point.lng;
-		else if(point.lng < minLng)
+		if(point.lng < minLng)
 			minLng = point.lng;
 	});
 	return {
@@ -223,18 +510,4 @@ function ChangeMapCenterByPointsAction(map)
 		var center = GetCenter(points);
 		map.setCenter(center);
 	}
-}
-
-function MapSightingsToMapAction(map)
-{
-	return function(sightings){
-		for(var i = 0;i < sightings.length;i++)
-		{
-			var marker = new google.maps.Marker({//Mark the sighting
-				position: {lat: sightings[i].lat, lng: sightings[i].lng},
-				map: map,
-				title: sightings[i].number + " " + sightings[i].speciesCommon + "(s)"
-			});
-		}
-	};
 }
